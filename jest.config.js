8a6b34e2506345ec9309669b6127f13a083ba8a2
Dror Wolmer <dror.wolmer@gmail.com>
2021-10-17 11:47:31 +0300

    Ignore example_project tests

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
+  testPathIgnorePatterns: ["/node_modules/", "example_project/"],
   preset: "ts-jest",
   restoreMocks: true,
   testEnvironment: "node",
