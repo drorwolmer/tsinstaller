88678d33a2f3fe7d0b9d5169de919ec6cfbd3441
leebm3 <leebm3@gmail.com>
2021-10-31 15:17:34 +0200

    reviewfix

--- tests/logger.test.ts
+++ tests/logger.test.ts
@@ -1,16 +1,13 @@
-import { log } from "../utils/logsHandler";
+import { log ,logDir} from "../utils/logsHandler";
 import {readFileSync,existsSync} from "fs";
-import {tmpdir} from "os";
-import {join} from "path"
 
 
 describe("Logger tests", () => {
     it("Log messages", async () => {
         const message = "logged message"
         log(message)
-        const dir = join(tmpdir(),"installer.log")
-        const content = readFileSync(dir);
+        const content = readFileSync(logDir);
         expect(content.toString()).toContain(message);
-        expect(existsSync(dir)).toBeTruthy();
+        expect(existsSync(logDir)).toBeTruthy();
     });
   });
--- utils/logsHandler.ts
+++ utils/logsHandler.ts
@@ -6,10 +6,11 @@ const timeStamp = () => {
     return new Date().toISOString().replace(/([^T]+)T([^\.]+).*/g, '$1 $2').replace(/[\s]/g,'_').replace(/[:]/g,'-');
   };
 const logFileName = `installer-${timeStamp()}.log`
-const logDir = join(tmpdir(), logFileName)
+export const logDir = join(tmpdir(), logFileName)
 
 export const log = (message: string) => {
     appendFileSync(logDir, `[INFO] ${timeStamp()}  ${message} \n`)
 }
+
 
 
