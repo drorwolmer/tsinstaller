a8ae36bb657945bf6bb4386707b54bd22c0fd1e1
leebm3 <leebm3@gmail.com>
2021-11-02 17:50:32 +0200

    logsfix

--- tests/logger.test.ts
+++ tests/logger.test.ts
@@ -6,9 +6,7 @@ describe("Logger tests", () => {
     it("Log messages", async () => {
         const message = "logged message"
         logToFile(message)
-        console.log(`${LOG_PATH}`)
         const content = readFileSync(LOG_PATH);
-        console.log(`${JSON.stringify(content.toString())}`)
         expect(content.toString()).toContain(message);
         expect(existsSync(LOG_PATH)).toBeTruthy();
     });
