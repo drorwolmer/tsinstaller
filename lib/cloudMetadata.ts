045f6282114f86f0b0f1a816e47c3a2169caacb3
leebm3 <leebm3@gmail.com>
2022-02-15 17:20:43 +0200

    cloudMetadata (#8)
    * cloudMetadata
    
    * Update cloudMetadata.ts
    
    * fetchCloudMetadata fix
    
    * fix reviews
    
    rename functions
    
    * Update cloudMetadata.ts
    
    * Update cloudMetadata.ts

---
+++ lib/cloudMetadata.ts
@@ -0,0 +1,89 @@
+import axios from "axios";
+import * as fs from "fs";
+
+const REQUEST_TIMEOUT = 10 * 1000;
+
+export const fetchAwsCustomMetadata = async <T>() => {
+  try {
+    const tokenResponse = await axios.put<string>(
+      `http://169.254.169.254/latest/api/token`,
+      {},
+      {
+        timeout: REQUEST_TIMEOUT,
+        headers: {
+          "X-aws-ec2-metadata-token-ttl-seconds": "21600",
+        },
+      }
+    );
+
+    const response = await axios.get<T>(
+      "http://169.254.169.254/latest/user-data",
+      {
+        timeout: REQUEST_TIMEOUT,
+        headers: {
+          "X-aws-ec2-metadata-token": tokenResponse.data,
+        },
+      }
+    );
+
+    return response.data;
+  } catch (e) {
+    return undefined;
+  }
+};
+
+export const fetchAzureCustomMetadata = async <T>() => {
+  try {
+    if (!fs.existsSync(`/var/lib/waagent/CustomData`)) {
+      return undefined;
+    }
+    const content = await fs.promises.readFile(
+      `/var/lib/waagent/CustomData`,
+      "base64"
+    );
+    const buff = Buffer.from(content, "base64");
+    const userdata = JSON.parse(buff.toString("utf-8")) as T;
+
+    return userdata;
+  } catch (e) {
+    return undefined;
+  }
+};
+
+export const fetchGcpCustomMetadata = async <T>() => {
+  let response;
+  try {
+    response = await axios.get<string>(
+      "http://metadata.google.internal/computeMetadata/v1/instance/attributes/customData",
+      {
+        timeout: REQUEST_TIMEOUT,
+        headers: { "Metadata-Flavor": "Google" },
+      }
+    );
+    const buff = Buffer.from(response.data, "base64");
+    const userdata = JSON.parse(buff.toString("utf-8")) as T;
+
+    return userdata;
+  } catch (e) {
+    return undefined;
+  }
+};
+
+export const getCustomMetadata = async <T>() => {
+  const [awsData, azureData, gcpData] = await Promise.all([
+    fetchAwsCustomMetadata<T>(),
+    fetchAzureCustomMetadata<T>(),
+    fetchGcpCustomMetadata<T>(),
+  ]);
+  if (awsData) {
+    return awsData;
+  }
+  if (azureData) {
+    return azureData;
+  }
+  if (gcpData) {
+    return gcpData;
+  }
+
+  return undefined;
+};
---
+++ tests/cloudMetadata.test.ts
@@ -0,0 +1,51 @@
+import MockAdapter from "axios-mock-adapter";
+import axios from "axios";
+import { getCustomMetadata } from "../lib/cloudMetadata";
+const axiosMock = new MockAdapter(axios);
+
+export type foo = {
+  bar: string;
+  baz: string;
+};
+
+const userData = {
+  bar: "connector",
+  baz: "netapp",
+};
+
+describe("cloud metadata tests", () => {
+  beforeEach(async () => {});
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  it("get user data - no metadata", async () => {
+    const result = await getCustomMetadata<foo>();
+    expect(result).toEqual(undefined);
+  });
+
+  it("get user data - google", async () => {
+    const buff = Buffer.from(JSON.stringify(userData), "utf-8");
+    const base64data = buff.toString("base64");
+    axiosMock
+      .onGet(
+        `http://metadata.google.internal/computeMetadata/v1/instance/attributes/customData`
+      )
+      .reply(200, base64data);
+
+    const result = await getCustomMetadata<foo>();
+    expect(result).toEqual(userData);
+  });
+
+  it("get user data - aws", async () => {
+    axiosMock
+      .onPut(`http://169.254.169.254/latest/api/token`)
+      .reply(200, "token");
+    axiosMock
+      .onGet(`http://169.254.169.254/latest/user-data`)
+      .reply(200, userData);
+
+    const result = await getCustomMetadata<foo>();
+    expect(result).toEqual(userData);
+  });
+});
