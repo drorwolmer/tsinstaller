121669afecc8e0e3f4ae60445bbce9595197365f
leebm3 <leebm3@gmail.com>
2022-03-22 11:13:50 +0200

    log_dir_fix

--- lib/logsHandler.ts
+++ lib/logsHandler.ts
@@ -1,18 +1,25 @@
-import {appendFileSync} from "fs";
-import {tmpdir} from "os";
-import {join} from "path";
-import moment from 'moment';
+import { appendFileSync, existsSync, mkdirSync } from "fs";
+import { tmpdir } from "os";
+import { join } from "path";
+import moment from "moment";
 
 export class Logger {
-    filePath: string;
-    
-    constructor(name?: string) {
-        const fileName = `${name || "installer"}-${moment(new Date()).format('YYYY-MM-DDThmmss')}.log`
-        this.filePath = join(tmpdir(), fileName)
-    }
+  filePath: string;
 
-    logToFile(message: string) {
-        appendFileSync(this.filePath, `[INFO] ${new Date().toISOString()} ${message}\n`)
+  constructor(name?: string) {
+    const fileName = `${name || "installer"}-${moment(new Date()).format(
+      "YYYY-MM-DDThmmss"
+    )}.log`;
+    if (!existsSync(join(tmpdir(), "logs"))) {
+      mkdirSync(join(tmpdir(), "logs"));
     }
+    this.filePath = join(tmpdir(), "logs", fileName);
+  }
 
+  logToFile(message: string) {
+    appendFileSync(
+      this.filePath,
+      `[INFO] ${new Date().toISOString()} ${message}\n`
+    );
+  }
 }
